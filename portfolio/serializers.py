b430ef40c455d889bf2e4056f810cc51156750d2
Arlefreak <arlefreak@gmail.com>
2016-02-04 19:33:00 -0600

    Added category serializer

--- portfolio/serializers.py
+++ portfolio/serializers.py
@@ -1,12 +1,11 @@
 from rest_framework import serializers
 from taggit_serializer.serializers \
     import TagListSerializerField, TaggitSerializer
-from .models import Project, ProjectCategory, Link
+from .models import Project, ProjectCategory, Link, LinkCategory
 
 
 class ProjectSerializer(TaggitSerializer, serializers.ModelSerializer):
     tags = TagListSerializerField(required=False)
-
     class Meta:
         model = Project
 
@@ -15,6 +14,11 @@ class ProjectCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = ProjectCategory
 
+class LinkCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LinkCategory
+
 class ProjectLinksSerializer(serializers.ModelSerializer):
+    category = LinkCategorySerializer(read_only=True)
     class Meta:
         model = Link
