b796824392f41f129eda25b0f011bfd004533ec7
Arlefreak <arlefreak@gmail.com>
2016-03-06 22:38:22 -0600

    Posts view updated

--- diary/urls.py
+++ diary/urls.py
@@ -4,7 +4,7 @@ from apiArlefreak.SharedRouter import SharedRootRouter
 
 router = SharedRootRouter()
 router.register(r'posts', views.PostViewSet)
-router.register(r'postImages', views.PostViewSet)
+router.register(r'postImages', views.PostImageViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
