1ef557331978ee0fdbd4dda35732837c499f91a7
afk <afk@ellugar.co>
2018-05-22 17:53:19 +0100

    improved taggit admin

--- ligoj/admin.py
+++ ligoj/admin.py
@@ -1,22 +1,32 @@
 from django.contrib import admin
+
 from .models import Link
+
 
 def LinkSetAct(modeladmin, request, queryset):
     # queryset.update(status='ACT')
     for obj in queryset:
         obj.status = 'ACT'
         obj.save()
+
+
 LinkSetAct.short_description = "Set Active"
+
 
 def LinkSetHidden(modeladmin, request, queryset):
     for obj in queryset:
         obj.status = 'HID'
         obj.save()
+
+
 LinkSetHidden.short_description = "Set Hidden"
+
 
 class LinkAdmin(admin.ModelAdmin):
     save_on_top = True
     actions = [LinkSetAct, LinkSetHidden]
+    date_hierarchy = 'date_created'
+    search_fields = ['tags__name']
     list_display = (
         'pk',
         'date_created',
@@ -35,9 +45,11 @@ class LinkAdmin(admin.ModelAdmin):
     ]
 
     def get_queryset(self, request):
-        return super(LinkAdmin, self).get_queryset(request).prefetch_related('tags')
+        return super(LinkAdmin,
+                     self).get_queryset(request).prefetch_related('tags')
 
     def tag_list(self, obj):
         return u", ".join(o.name for o in obj.tags.all())
+
 
 admin.site.register(Link, LinkAdmin)
