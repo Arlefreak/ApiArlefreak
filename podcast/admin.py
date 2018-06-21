ba5daae91d21edd67e0e84ea4d610b380f44390b
afk <afk@ellugar.co>
2018-06-20 20:49:50 -0500

    Add admin config:

--- podcast/admin.py
+++ podcast/admin.py
@@ -1,34 +1,35 @@
-from django.contrib import admin
-from .models import *
 from adminsortable.admin import SortableAdmin, SortableTabularInline
+from django.contrib import admin
+
+from .models import *
+
 
 class AdminImageMixin(object):
     def admin_image(self, obj):
         return u"<img src='%s' style='height: 100px; width: auto; display: block'/>" % obj.image.url
+
     admin_image.allow_tags = True
     admin_image.short_description = u"Preview"
 
+
 class ViewOnSiteMixin(object):
     def view_on_site(self, obj):
-        return u"<a class='button' href='%s'>view on site</a>" % obj.get_absolute_url()
+        return u"<a class='button' href='%s'>view on site</a>" % obj.get_absolute_url(
+        )
+
     view_on_site.allow_tags = True
     view_on_site.short_description = u"View on site"
+
 
 @admin.register(Podcast)
 class PodcastAdmin(SortableAdmin, AdminImageMixin, ViewOnSiteMixin):
     save_as = True
     fieldsets = (
         (None, {
-            'fields': (
-                'title',
-                'image'
-            )
+            'fields': ('title', 'image')
         }),
         ('Author', {
-            'fields': (
-                'author',
-                'author_mail'
-            )
+            'fields': ('author', 'author_mail')
         }),
         ('Categories', {
             'fields': (
@@ -47,6 +48,7 @@ class PodcastAdmin(SortableAdmin, AdminImageMixin, ViewOnSiteMixin):
                 'website',
                 'episodesUrl',
                 'iTunesURL',
+                'googlePodcast',
                 'feedBurner',
             )
         }),
@@ -72,16 +74,13 @@ class PodcastAdmin(SortableAdmin, AdminImageMixin, ViewOnSiteMixin):
         'admin_image',
     ]
 
+
 @admin.register(Episode)
 class EpisodeAdmin(SortableAdmin, AdminImageMixin, ViewOnSiteMixin):
     save_as = True
     fieldsets = (
         (None, {
-            'fields': (
-                'podcast',
-                'title',
-                'image'
-            )
+            'fields': ('podcast', 'title', 'image')
         }),
         ('Files', {
             'fields': (
@@ -118,6 +117,4 @@ class EpisodeAdmin(SortableAdmin, AdminImageMixin, ViewOnSiteMixin):
         'duration',
         'admin_image',
     ]
-    list_filter = (
-        'podcast',
-    )
+    list_filter = ('podcast', )
